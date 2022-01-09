1adbdaf6e63b8043b92fd66db4e47c2bdbfc5593
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2022-01-09 12:29:38 +0100

    correct variable names for PgConnect

--- pkg/database/dbconnect.go
+++ pkg/database/dbconnect.go
@@ -8,8 +8,9 @@ import (
 )
 
 // PgConnect connects to a pg database
-func PgConnect(db_name, user_name, db_password string) (*sqlx.DB, error) {
-	db, err := sqlx.Connect("postgres", fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", user_name, db_name, db_password))
+func PgConnect(dbName, userName, dbPassword string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres",
+		fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", userName, dbName, dbPassword))
 	if err != nil {
 		return nil, fmt.Errorf("internal/database/connect PgConnect: %v", err)
 	}
